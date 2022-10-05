aef2cf1bd92a228fd94d80386c8612e3d12c38ac
Serges01 <38444929+Serges01@users.noreply.github.com>
2022-10-05 15:37:56 +0200

    ver1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
-  }
+   } 
   try {
   } catch (ExceptionInteressante e )
   
